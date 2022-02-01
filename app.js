b4d610ee03d188508fa8d83900c812c5107b835e
Ryan <ryangibson365@gmail.com>
2022-01-31 18:17:41 -0600

    simple scafolding for compile yargs

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 const yargs = require('yargs');
 const chalk = require('chalk')
+
+yargs.command({
+    command: 'compile',
+    describe: 'Compile a solidity contract, or a set of contracts',
+    aliases: 'C',
+    builder: {
+        contracts: {
+            describe: 'Name/title of contract or contracts. For multiple contracts seperate by a comma and a space EX: <"contract1, contract2">',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log(argv.contracts)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    aliases: 'ls',
+    describe: 'List all the contracts available',
+    handler: () => {
+        console.log(chalk.green('list contracts'));
+    }
+});
+
+
+yargs.parse(process.argv.slice(2));
