02bf472d6fb19c774b2bf9cef11bad3efb4bff24
Ryan <ryangibson365@gmail.com>
2022-02-07 18:23:54 -0600

    start Auction.sol contract development

---
+++ contracts/Auction.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Auction {
+    address payable public owner;
+    //to derive start and end time of the auction
+    uint public startBlock;
+    uint public endBlock;
+    //storage for inter planatary file system. cheaper than on chain storage
+    string public ipfsHash;
+
+    enum State {
+        Started,
+        Running,
+        Ended,
+        Canceled
+    }
+    //enum state of the action
+    State public auctionState;
+
+    uint public highestBid;
+    //address is payable in case auction is prematuraly canceled
+    address payable public highestBidder;
+    mapping(address => uint) public bids;
+    uint bidIncrement;
+
+    //
+    modifier onlyOwner(){
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier notOwner(){
+        require(msg.sender != owner);
+        _;
+    }
+
+    modifier afterStart(){
+        require(block.number >= startBlock);
+        _;
+    }
+
+    modifier beforeEnd(){
+        require(block.number <= endBlock);
+        _;
+    }
+
+    modifier auctionRunning(){
+        require(auctionState == State.Running);
+        require(block.number >= startBlock);
+        require(block.number <= endBlock);
+        _;
+    }
+
+    constructor() {
+        owner = payable(msg.sender);
+        auctionState = State.Running;
+        //based off ether ~15s block time
+        startBlock = block.number;
+        //40320 is roughly 1week
+        endBlock = startBlock +40320;
+        ipfsHash = '';
+        //100 wei
+        bidIncrement = 100;
+    }
+
+
+
+
+}
---
+++ contracts/test/auction.test.js
@@ -0,0 +1,20 @@
+import chai from 'chai';
+import Ganache from 'ganache';
+import Web3 from 'web3';
+import { compile } from './util.js';
+
+const chaiAssert = chai.assert;
+
+const web3 = new Web3(Ganache.provider());
+
+let accounts;
+let contract;
+
+beforeEach(async () => {
+    const { bytecode, abi } = compile('Auction.sol');
+    accounts = await web3.eth.getAccounts();
+
+    contract = await new web3.eth.Contract(abi)
+        .deploy({ data: bytecode, arguments: [] })
+        .send({ from: accounts[0], gas: '1000000' });
+});
